39ed06dc28268999a0802df0a20a33d25659191f
Nadine Rosete <nadine.rosete@yale.edu>
2025-02-28 19:25:22 -0500

    Slice Reorder to Split Into Single Frames

--- dicom2frame.py
+++ dicom2frame.py
@@ -13,9 +13,7 @@ def load_and_split(path):
     for frame in range(len(input_scan_frames)):
         new_nifti = nib.Nifti1Image(input_scan_frames[frame], input_scan.affine, new_header)
         filename = path.split('.')[0]
-        nib.save(new_nifti, f'{filename}_frame{frame+1}.nii.gz')
-
-for sub in subjects:
-    for file in paths[sub]:
-        print(f'process file {file}')
-        load_and_split(file)
+        nib.save(new_nifti, f'PEXA^9_Retro_CTA_LV+RV4A_0-50-5_frame_{frame}.nii.gz')
+load_and_split("/mnt/c/Users/Nadin/OneDrive/Documents/Computational_Biomechanics_Lab/Repos/compbiomechproject/4D_CT/PEXA9 ExVivo CTA RV+LV+T3 RetroA/PEXA^9_CTA_RV+LV+TANK_3_RetroA.nii.gz")
+# Step 1:use command in slack that anne sent to convert dicom to nifti (dcm2niix -z y -o . -f '%n_%d' -x i -y n .)
+# Step 2: use this script and edit the above file names and directories to get the individual ED frame that you need
